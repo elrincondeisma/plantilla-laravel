33c2483ad2995c63e035f26fcf57d089bbf51b19
Ismael Catala <ismael.catala@smileandlearn.com>
2024-09-16 19:22:46 +0200

    feat:

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 ![El Rincón de Isma](/public/assets/images/banner.jpg "La plantilla definitiva")
 # El Rincón de Isma - Plantilla Definitiva de Laravel
+Este es un proyecto basado en [Laravel](https://laravel.com/), un framework PHP para el desarrollo de aplicaciones web. A continuación se describen los pasos para la instalación y configuración inicial del proyecto.
+## Requisitos previos
+
+Antes de instalar Laravel, asegúrate de tener los siguientes requisitos:
+
+- **PHP >= 8.1** (con las siguientes extensiones habilitadas: `BCMath`, `Ctype`, `Fileinfo`, `JSON`, `Mbstring`, `OpenSSL`, `PDO`, `Tokenizer`, `XML`)
+- **Composer** (para gestionar dependencias de PHP)
+- **MySQL/MariaDB/PostgreSQL** (o cualquier otro sistema de gestión de bases de datos compatible)
+- **Servidor Web** (Apache, Nginx, etc.)
+- **Node.js** y **npm/yarn** (para gestionar paquetes de frontend si se usan)
+
+## Instalación
+
+Sigue los pasos para clonar el proyecto e instalar las dependencias:
+
+### 1. Clonar el repositorio
+
+Clona este repositorio en tu máquina local:
+```bash
+git clone https://github.com/elrincondeisma/plantilla-laravel
+cd plantilla-laravel
+```
+
+### 2. Instalar dependencias
+
+Instala las dependencias necesarias para el proyecto:
+```bash
+composer install
+npm install
+npm run build
+cp .env.example .env
+php artisan key:generate
+```
+
+## Configuración
+
+Sigue los pasos para configurar el proyecto:
+
+### 1. Corre las migraciones
+```bash
+php artisan migrate
+```
+
+### 2. Crea la cuenta de administrador de FilamentPHP
+```bash
+php artisan hexa:account --create
+```
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,15 +38,15 @@
 
                     <!-- Buttons -->
                     <div class="mt-8 gap-3 flex justify-center">
-                        <button type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
+                        <a type="button" href="/login" class="cursor-pointer py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
                             Login
                             <x-tabler-brand-youtube />
-                          </button>
-                          <button type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700">
+                          </a>
+                          <a type="button" href="/register" class="cursor-pointer py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700">
                             Register
                             <x-tabler-mood-plus />
 
-                          </button>
+                          </a>
                     </div>
                     <div class="flex justify-center mt-4">
                         <x-dark-toggle-button />
